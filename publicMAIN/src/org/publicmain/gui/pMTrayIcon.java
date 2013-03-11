854e341e7ae643feb16e6b99039d259f9ed04410
martinszymczak <martinszymczak@gmx.de>
2013-03-11 20:24:42 +0100

    TrayIcon eingefügt

---
+++ publicMAIN/src/org/publicmain/gui/pMTrayIcon.java
@@ -0,0 +1,188 @@
+package org.publicmain.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class pMTrayIcon {
+    public static void main(String[] args) {
+        /* Use an appropriate Look and Feel */
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+        //Schedule a job for the event-dispatching thread:
+        //adding TrayIcon.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    
+    private static void createAndShowGUI() {
+        //Check the SystemTray support
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray is not supported");
+            return;
+        }
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon =
+                new TrayIcon(createImage("/pM.gif", "tray icon"));
+        final SystemTray tray = SystemTray.getSystemTray();
+        
+        // Create a popup menu components
+        MenuItem aboutItem = new MenuItem("Mein Fenster");
+        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
+        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
+        Menu displayMenu = new Menu("Display");
+        MenuItem errorItem = new MenuItem("Error");
+        MenuItem warningItem = new MenuItem("Warning");
+        MenuItem infoItem = new MenuItem("Info");
+        MenuItem noneItem = new MenuItem("None");
+        MenuItem exitItem = new MenuItem("Exit");
+        
+        //Add components to popup menu
+        popup.add(aboutItem);
+        popup.addSeparator();
+        popup.add(cb1);
+        popup.add(cb2);
+        popup.addSeparator();
+        popup.add(displayMenu);
+        displayMenu.add(errorItem);
+        displayMenu.add(warningItem);
+        displayMenu.add(infoItem);
+        displayMenu.add(noneItem);
+        popup.add(exitItem);
+        
+        trayIcon.setPopupMenu(popup);
+        
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+            return;
+        }
+        
+        trayIcon.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                        "This dialog box is run from System Tray");
+            }
+        });
+        
+        aboutItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+//                JOptionPane.showMessageDialog(null,
+//                        "This dialog box is run from the About menu item");
+        		JFrame fenster = new JFrame();
+        		fenster.setTitle("Mein Fenster");
+        		JButton button = new JButton("Schaltfläche");
+        		fenster.add(button);
+        		fenster.setLayout(new FlowLayout());
+        		fenster.setSize(400,500);
+        		fenster.setVisible(true);
+        		fenster.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        		BufferedImage image;
+				try {
+					image = ImageIO.read(fenster.getClass().getResource("/bulb.gif"));
+					fenster.setIconImage(image);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+                
+            	
+            }
+        });
+        
+        cb1.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                int cb1Id = e.getStateChange();
+                if (cb1Id == ItemEvent.SELECTED){
+                    trayIcon.setImageAutoSize(true);
+                } else {
+                    trayIcon.setImageAutoSize(false);
+                }
+            }
+        });
+        
+        cb2.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                int cb2Id = e.getStateChange();
+                if (cb2Id == ItemEvent.SELECTED){
+                    trayIcon.setToolTip("Sun TrayIcon");
+                } else {
+                    trayIcon.setToolTip(null);
+                }
+            }
+        });
+        
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                MenuItem item = (MenuItem)e.getSource();
+                //TrayIcon.MessageType type = null;
+                System.out.println(item.getLabel());
+                if ("Error".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.ERROR;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "René", TrayIcon.MessageType.ERROR);
+                    
+                } else if ("Warning".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.WARNING;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "Tobi", TrayIcon.MessageType.WARNING);
+                    
+                } else if ("Info".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.INFO;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "Alfred", TrayIcon.MessageType.INFO);
+                    
+                } else if ("None".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.NONE;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "Martin", TrayIcon.MessageType.NONE);
+                }
+            }
+        };
+        
+        errorItem.addActionListener(listener);
+        warningItem.addActionListener(listener);
+        infoItem.addActionListener(listener);
+        noneItem.addActionListener(listener);
+        
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tray.remove(trayIcon);
+                System.exit(0);
+            }
+        });
+    }
+    
+    //Obtain the image URL
+    protected static Image createImage(String path, String description) {
+        URL imageURL = pMTrayIcon.class.getResource(path);
+        
+        if (imageURL == null) {
+            System.err.println("Resource not found: " + path);
+            return null;
+        } else {
+            return (new ImageIcon(imageURL, description)).getImage();
+        }
+    }
+}

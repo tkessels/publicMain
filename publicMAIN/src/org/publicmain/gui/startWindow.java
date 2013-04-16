e6b2cb3a86c1340e5f531772420c709a85d56454
rpfaffner <publicmain13@gmail.com>
2013-04-16 09:31:26 +0200

    Startwindow funktionierenden blockenden aufruf hinzugefügt

--- publicMAIN/src/org/publicmain/gui/startWindow.java
+++ publicMAIN/src/org/publicmain/gui/startWindow.java
@@ -15,10 +15,9 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class startWindow {
+public class startWindow extends JFrame implements ActionListener{
 
-	private static startWindow me;
-	private JFrame startWindowFrame;
+	private startWindow instanz;
 	private JLabel welcomeLogo;
 	private JLabel welcomeLabel1;
 
@@ -27,13 +26,23 @@ public class startWindow {
 	private JButton goButton;
 	private JButton pullButton;
 	
+	private JLabel wellcomeLabel2;
+	private JLabel wellcomeLabel3;
+	private JLabel userNameLabel;
+	private JTextField userNameTextField;
+	private JLabel passWordLabel;
+	private JPasswordField passWordTextField;
+	private JTextField statusTextField;
+	private JLabel backupserverIPLabel;
+	private JTextField backupserverIPTextField;
+	
 	private GridBagConstraints c;
 	private Insets set;
 	private boolean pushedGo;	//TODO: Diese Variante zu prüfen ob startWindow "fertig" ist gefällt mir (noch) nicht -> aber gerade keine andere idee ist ja schon spät :-)
 	
 	private startWindow() {
 		
-		this.startWindowFrame		=	new JFrame();
+		instanz =this;
 		this.welcomeLogo			= 	new JLabel(new ImageIcon(getClass().getResource("textlogo.png")));
 		this.welcomeLabel1			=	new JLabel("Enter your Nick an push \"GO\" if you just want to chat.");
 		this.nickNameLabel			=	new JLabel("Nickname");
@@ -46,102 +55,102 @@ public class startWindow {
 		this.set 					=	new Insets(5, 5, 5, 5);
 		
 		this.pushedGo				=   false;	//TODO: Diese Variante zu prüfen ob startWindow "fertig" ist gefällt mir (noch) nicht -> aber gerade keine andere idee ist ja schon spät :-)
+
+		this.goButton.addActionListener(this);
+		this.pullButton.addActionListener(this);
 		
-		this.goButton.addActionListener(new startWindowButtonController(startWindowFrame, c, goButton));
-		this.pullButton.addActionListener(new startWindowButtonController(startWindowFrame, c, goButton));
+		this.setTitle("Welcome!");
+		this.setIconImage(new ImageIcon(getClass().getResource("pM_Logo2.png")).getImage());
+		this.getContentPane().setBackground(Color.WHITE);
+		this.setMinimumSize(new Dimension(200, 180));
 		
-		startWindowFrame.setTitle("Welcome!");
-		startWindowFrame.setIconImage(new ImageIcon(getClass().getResource("pM_Logo2.png")).getImage());
-		startWindowFrame.getContentPane().setBackground(Color.WHITE);
-		startWindowFrame.setMinimumSize(new Dimension(200, 180));
-		
-		startWindowFrame.setLayout(new GridBagLayout());
+		this.setLayout(new GridBagLayout());
 		c.insets 	= set;
 		c.fill 		= GridBagConstraints.HORIZONTAL;
 		c.anchor	= GridBagConstraints.LINE_START;
 		
-		// hinzufügen der Komponenten zum startWindowFrame
+		// hinzufügen der Komponenten zum this
 		c.gridx 	= 0;
 		c.gridy 	= 0;
 		c.gridwidth = 2;
-		startWindowFrame.add(welcomeLogo ,c);
+		this.add(welcomeLogo ,c);
 		
 		c.gridy 	= 1;
-		startWindowFrame.add(welcomeLabel1, c);
+		this.add(welcomeLabel1, c);
 		
 		c.gridy 	= 2;
 		c.gridwidth = 1;
-		startWindowFrame.add(nickNameLabel, c);
+		this.add(nickNameLabel, c);
 		
 		c.gridx 	= 1;
-		startWindowFrame.add(nickNameTextField, c);
+		this.add(nickNameTextField, c);
 		
 		c.gridx 	= 0;
 		c.gridy 	= 3;
-		startWindowFrame.add(goButton, c);
+		this.add(goButton, c);
 		
 		c.gridx 	= 1;
-		startWindowFrame.add(pullButton, c);
+		this.add(pullButton, c);
 		
-		startWindowFrame.pack();
-		startWindowFrame.setResizable(false);
-		startWindowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		startWindowFrame.setLocationRelativeTo(null);
-		startWindowFrame.setVisible(true);
+		this.pack();
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+	
+	private synchronized boolean bla(){
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
 	}
 
-	public static startWindow getStartWindow(){
-		if (me == null) {
-			me = new startWindow();
-		}
-		return me;
+	public static boolean getStartWindow(){
+		startWindow me = new startWindow();
+		boolean x = me.bla();
+		me.dispose();
+		return x;
+
+		
 	}
+
 	public boolean isGoPushed(){
 		return pushedGo;
 	}
 //TODO: noch komplett umbauen da jetzt ?Elementklasse? und somit zugriff auf variablen - > vorher seperate Klasse
-	class startWindowButtonController implements ActionListener{
-		private JFrame startWindowFrame;
-		private JLabel wellcomeLabel2;
-		private JLabel wellcomeLabel3;
-		private JLabel userNameLabel;
-		private JTextField userNameTextField;
-		private JLabel passWordLabel;
-		private JPasswordField passWordTextField;
-		private JTextField statusTextField;
-		private JLabel backupserverIPLabel;
-		private JTextField backupserverIPTextField;
-		private GridBagConstraints c;
-		
-		private JButton goButton;
-		
-		public startWindowButtonController(JFrame startWindowFrame, GridBagConstraints c, JButton goButton) {
-			this.startWindowFrame = startWindowFrame;
-			this.goButton = goButton;
-			this.c = c;
-		}
-	
-		public void actionPerformed(ActionEvent evt) {
+@Override
+	public void actionPerformed(ActionEvent evt) {
+		synchronized (instanz) {
+			//TODO: evtl. boolean setezen dessen status in ner methode aus der Starterklasse abgerufen werden kann.
+			
+			
 			
 			JButton sourceButton = (JButton)evt.getSource();
 			switch(sourceButton.getText()){
-			
 			case "GO":
 				//TODO: Daten überprüfen, in .cfg setzen
-				startWindowFrame.setVisible(false);
+				this.setVisible(false);
 				pushedGo = true;//TODO: Diese Variante gefällt mir (noch) nicht -> aber keina andere idee
-				//TODO: evtl. boolean setezen dessen status in ner methode aus der Starterklasse abgerufen werden kann.
+				synchronized (instanz) {
+					//TODO: evtl. boolean setezen dessen status in ner methode aus der Starterklasse abgerufen werden kann.
+					this.notifyAll();
+				}
 				break;
+				
 			case "PULL from Backup":
-				this.wellcomeLabel2			=	new JLabel("For using backupserver enter Username, Password");
-				this.wellcomeLabel3			=	new JLabel("and the IP of your backupserver");
-				this.userNameLabel			=	new JLabel("Username");
-				this.userNameTextField 		=	new JTextField();
-				this.passWordLabel			=	new JLabel("Password");
-				this.passWordTextField		=	new JPasswordField();
-				this.statusTextField		=	new JTextField();
-				this.backupserverIPLabel	=	new JLabel("Backupserver IP");
-				this.backupserverIPTextField=	new JTextField();
+				wellcomeLabel2			=	new JLabel("For using backupserver enter Username, Password");
+				wellcomeLabel3			=	new JLabel("and the IP of your backupserver");
+				userNameLabel			=	new JLabel("Username");
+				userNameTextField 		=	new JTextField();
+				passWordLabel			=	new JLabel("Password");
+				passWordTextField		=	new JPasswordField();
+				statusTextField			=	new JTextField();
+				backupserverIPLabel		=	new JLabel("Backupserver IP");
+				backupserverIPTextField=	new JTextField();
 				
 				statusTextField.setBackground(new Color(229, 195, 0));
 				statusTextField.setEditable(false);
@@ -150,57 +159,58 @@ public class startWindow {
 				c.gridx 	= 0;
 				c.gridy 	= 3;
 				c.gridwidth = 2;
-				startWindowFrame.add(goButton, c);
+				this.add(goButton, c);
 				
 				c.gridy 	= 4;
 				c.gridx 	= 0;
 				c.gridwidth = 2;
-				startWindowFrame.add(wellcomeLabel2, c);
+				this.add(wellcomeLabel2, c);
 				
 				c.gridy 	= 5;
-				startWindowFrame.add(wellcomeLabel3, c);
+				this.add(wellcomeLabel3, c);
 				
 				c.gridx 	= 0;
 				c.gridy 	= 6;
 				c.gridwidth = 1;
-				startWindowFrame.add(userNameLabel, c);
+				this.add(userNameLabel, c);
 				
 				c.gridx 	= 1;
-				startWindowFrame.add(userNameTextField, c);
+				this.add(userNameTextField, c);
 				
 				c.gridx 	= 0;
 				c.gridy 	= 7;
-				startWindowFrame.add(passWordLabel, c);
+				this.add(passWordLabel, c);
 				
 				c.gridx 	= 1;
-				startWindowFrame.add(passWordTextField, c);
+				this.add(passWordTextField, c);
 				
 				c.gridx 	= 0;
 				c.gridy 	= 8;
-				startWindowFrame.add(backupserverIPLabel, c);
+				this.add(backupserverIPLabel, c);
 				
 				c.gridx 	= 1;
-				startWindowFrame.add(backupserverIPTextField, c);
+				this.add(backupserverIPTextField, c);
 				
 				c.gridx 	= 0;
 				c.gridy 	= 9;
 				c.gridwidth = 2;
-				startWindowFrame.add(statusTextField, c);
+				this.add(statusTextField, c);
 				
 				c.gridx 	= 0;
 				c.gridy 	= 10;
 				c.gridwidth = 2;
-				startWindowFrame.add(sourceButton, c);
+				this.add(sourceButton, c);
 				sourceButton.setVisible(true);
 				
-				startWindowFrame.pack();
+				this.pack();
 			break;
 			
 			case "PULL from Backup & GO":						//Vorsicht...ist ein bissl undurchsichtig! Ist der selbe Button wie "oben" nur wurde der Text
 				System.out.println("wenn das klappt geht´s");	//umbenannt und neu positioniert. Daher trifft ein anderer case im (selben) Buttoncontroller zu.
 				// Hier noch viel sinnvolles implementieren!!!
-				break;
-			}
-		}
+				this.notifyAll();
+			break;
+		}	
 	}
+}
 }
--- publicMAIN/src/publicMAIN.java
+++ publicMAIN/src/publicMAIN.java
@@ -22,25 +22,11 @@ public class publicMAIN {
 	public static void main(String[] args) throws IOException {
 		if(Config.getConfig().getLock()){
 			if(Config.getConfig().getAlias() == null){
-				
-				startWindow sw = startWindow.getStartWindow();
-				while(!sw.isGoPushed()){	//TODO: Diese Variante zu prüfen ob startWindow "fertig" ist gefällt mir (noch) nicht -> aber gerade keine andere idee ist ja schon spät :-)
-					try {
-						Thread.sleep((long)1000);
-					} catch (InterruptedException e) {
-						LogEngine.log("Fehler im warteThreat: " + e.getMessage(), LogEngine.ERROR);
-					}
-				}
+				boolean tom = startWindow.getStartWindow();
 				//TODO: hier darauf warten bis nutzer daten eingegeben und "submit" gedrückt hat. also zum Beispiel ein boolean im Startwindwo überprüfen
 				//TODO: Will werte bei Submit-Click im Startwindow in config speichern...wie? ;-)
 				
-				//TODO: Warum läuft das prog nicht direkt weiter wenn der DB-Server aus ist? Selbst wenn dieser Aufruf hier in nem extra Threat ist geht´s nicht.
-				//Du hattest das ja nie in einen Thread gepackt sonder lediglich in ein Runnable und dann die run Methode selbst ausgeführt anstatt DAS von einem Thread machen zu lassen
-				//habe das im DB Construktor korrigiert ... den Rest schau ich mir später an viel Spaß beim planschen  ;)
 				LocalDBConnection.getDBConnection(); 
-				
-				
-
 				GUI.getGUI();
 			}else {
 				LocalDBConnection.getDBConnection(); //TODO: Warum läuft das prog nicht direkt weiter wenn der DB-Server aus ist? Selbst wenn dieser Aufruf hier in nem extra Threat ist geht´s nicht.

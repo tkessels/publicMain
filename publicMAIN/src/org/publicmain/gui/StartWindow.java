56cfb7adcf7c2b09e735a1cd44af76776401ea5c
rpfaffner <publicmain13@gmail.com>
2013-04-17 14:32:41 +0200

    some little changes

--- publicMAIN/src/org/publicmain/gui/StartWindow.java
+++ publicMAIN/src/org/publicmain/gui/StartWindow.java
@@ -7,8 +7,6 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.regex.Matcher;
@@ -23,6 +21,7 @@ import javax.swing.JTextField;
 
 import org.images.Help;
 import org.publicmain.common.Config;
+import org.publicmain.common.LogEngine;
 
 public class StartWindow extends JFrame implements ActionListener{
 
@@ -141,8 +140,7 @@ public class StartWindow extends JFrame implements ActionListener{
 		try {
 			this.wait();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LogEngine.log(this, "Fehler in der syncorinized Methode" + e.getMessage(), LogEngine.ERROR);
 		}
 		return plsRunGUI;
 	}
@@ -152,8 +150,60 @@ public class StartWindow extends JFrame implements ActionListener{
 		boolean x = me.bla();
 		me.dispose();
 		return x;
-
+	}
+	
+	private void changeStructure(JButton sourceButton){
+		statusTextField.setBackground(new Color(229, 195, 0));
+		statusTextField.setEditable(false);
+		sourceButton.setText("PULL from Backup & GO");
 		
+		c.gridx 	= 0;
+		c.gridy 	= 3;
+		c.gridwidth = 2;
+		this.add(goButton, c);
+		
+		c.gridy 	= 4;
+		c.gridx 	= 0;
+		c.gridwidth = 2;
+		this.add(wellcomeLabel2, c);
+		
+		c.gridy 	= 5;
+		this.add(wellcomeLabel3, c);
+		
+		c.gridx 	= 0;
+		c.gridy 	= 6;
+		c.gridwidth = 1;
+		this.add(userNameLabel, c);
+		
+		c.gridx 	= 1;
+		this.add(userNameTextField, c);
+		
+		c.gridx 	= 0;
+		c.gridy 	= 7;
+		this.add(passWordLabel, c);
+		
+		c.gridx 	= 1;
+		this.add(passWordTextField, c);
+		
+		c.gridx 	= 0;
+		c.gridy 	= 8;
+		this.add(backupserverIPLabel, c);
+		
+		c.gridx 	= 1;
+		this.add(backupserverIPTextField, c);
+		
+		c.gridx 	= 0;
+		c.gridy 	= 9;
+		c.gridwidth = 2;
+		this.add(statusTextField, c);
+		
+		c.gridx 	= 0;
+		c.gridy 	= 10;
+		c.gridwidth = 2;
+		this.add(sourceButton, c);
+		sourceButton.setVisible(true);
+		
+		this.pack();
 	}
 
 	public void actionPerformed(ActionEvent evt) {
@@ -190,64 +240,10 @@ public class StartWindow extends JFrame implements ActionListener{
 						}
 						this.setVisible(false);
 					}
-					
-					
 					break;
 					
 				case "PULL from Backup":
-
-					
-					statusTextField.setBackground(new Color(229, 195, 0));
-					statusTextField.setEditable(false);
-					sourceButton.setText("PULL from Backup & GO");
-					
-					c.gridx 	= 0;
-					c.gridy 	= 3;
-					c.gridwidth = 2;
-					this.add(goButton, c);
-					
-					c.gridy 	= 4;
-					c.gridx 	= 0;
-					c.gridwidth = 2;
-					this.add(wellcomeLabel2, c);
-					
-					c.gridy 	= 5;
-					this.add(wellcomeLabel3, c);
-					
-					c.gridx 	= 0;
-					c.gridy 	= 6;
-					c.gridwidth = 1;
-					this.add(userNameLabel, c);
-					
-					c.gridx 	= 1;
-					this.add(userNameTextField, c);
-					
-					c.gridx 	= 0;
-					c.gridy 	= 7;
-					this.add(passWordLabel, c);
-					
-					c.gridx 	= 1;
-					this.add(passWordTextField, c);
-					
-					c.gridx 	= 0;
-					c.gridy 	= 8;
-					this.add(backupserverIPLabel, c);
-					
-					c.gridx 	= 1;
-					this.add(backupserverIPTextField, c);
-					
-					c.gridx 	= 0;
-					c.gridy 	= 9;
-					c.gridwidth = 2;
-					this.add(statusTextField, c);
-					
-					c.gridx 	= 0;
-					c.gridy 	= 10;
-					c.gridwidth = 2;
-					this.add(sourceButton, c);
-					sourceButton.setVisible(true);
-					
-					this.pack();
+					changeStructure(sourceButton);
 				break;
 				
 				case "PULL from Backup & GO":						//Vorsicht...ist ein bissl undurchsichtig! Ist der selbe Button wie "oben" nur wurde der Text
@@ -284,7 +280,7 @@ public class StartWindow extends JFrame implements ActionListener{
 						this.setVisible(false);
 					}
 				break;
-		}	
+			}	
+		}
 	}
-}
 }

15c5ed6245a5958237ff396a10ce1be071e926fe
martinszymczak <martinszymczak@gmx.de>
2013-05-02 20:20:06 +0200

    Kanal kommentiert

--- publicMAIN/src/org/publicmain/chatengine/Kanal.java
+++ publicMAIN/src/org/publicmain/chatengine/Kanal.java
@@ -4,6 +4,8 @@ import java.util.Observable;
 import org.publicmain.common.MSG;
 
 /**
+ * Abstrakte Klasse von welcher weitere Kanäle abgeleitet werden können.
+ * 
  * @author ATRM
  * 
  */
@@ -11,43 +13,55 @@ import org.publicmain.common.MSG;
 public abstract class Kanal extends Observable {
 	protected final Object referenz;
 
-	//protected TreeSet<MSG> messages = new TreeSet<MSG>();
+//	Ggf. für die weitere Entwicklung benötigt.
+//	protected TreeSet<MSG> messages = new TreeSet<MSG>();
 
+	/**
+	 * Abstrakte Methode zum hinzufügen einer Nachricht zu einem Kanal.
+	 * 
+	 * @param nachricht
+	 * @return boolean
+	 */
 	public abstract boolean add(MSG nachricht);
 
 	/**
-	 * TODO: Kommentar
+	 * TODO: Prüfen ob die Grütze stimmt, die ich hier zu besten gebe. Hält eine
+	 * Referenz auf diese Instanz des Kanals.
+	 * 
+	 * @param reference
 	 */
 	public Kanal(Object reference) {
 		this.referenz = reference;
 	}
 
-	/**
-	 * Gibt die letzten x Nachrichten aus einem Kanal als Set zurück
-	 * 
-	 * @param count, die gewünschte Anzahl von Nachrichten
-	 * @return, Set der x-Letzen Nachrichten
-	 */
-	/*public Set<MSG> getLastMSGs(int count) {
-		if (messages.size() >= count) {
-			Iterator<MSG> x = messages.descendingIterator();
-			MSG last = messages.last();
-			while (x.hasNext() && (count > 0)) {
-				count--;
-				last = ((MSG) x.next());
-			}
-			return messages.tailSet(last, true);
-		}
-		return messages;
-	}*/
-/*
-	*//**
-	 * Löscht alle Nachrichten aus dem Kanal.
-	 *//*
-	public void purgeMSGs() {
-		messages.clear();
-	}
-	*/
+//	 Ggf. für die weitere Entwicklung benötigt.
+//	/**
+//	 * Gibt die letzten x Nachrichten aus einem Kanal als Set zurück
+//	 * 
+//	 * @param count, die gewünschte Anzahl von Nachrichten
+//	 * @return, Set der x-Letzen Nachrichten
+//	 */
+//	public Set<MSG> getLastMSGs(int count) {
+//		if (messages.size() >= count) {
+//			Iterator<MSG> x = messages.descendingIterator();
+//			MSG last = messages.last();
+//			while (x.hasNext() && (count > 0)) {
+//				count--;
+//				last = ((MSG) x.next());
+//			}
+//			return messages.tailSet(last, true);
+//		}
+//		return messages;
+//	}
+	
+//	 Ggf. für die weitere Entwicklung benötigt.
+//	/**
+//	 * Löscht alle Nachrichten aus dem Kanal.
+//	 */
+//	public void purgeMSGs() {
+//		messages.clear();
+//	}
+
 	/**
 	 * TODO: Kommentar
 	 */
@@ -68,7 +82,7 @@ public abstract class Kanal extends Observable {
 	}
 
 	/**
-	 * TODO: Kommentar
+	 * Getter für die Referenz.
 	 */
 	public Object getReferenz() {
 		return referenz;

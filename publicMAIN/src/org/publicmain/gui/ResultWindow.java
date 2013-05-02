2085633de27adff5dc63f87bfc7df0749b0e9fb9
aberthold <arth004@gmail.com>
2013-05-02 17:24:04 +0200

    ResultWindow: Konstruktor das ein Objekt[][] entgegennimmt

--- publicMAIN/src/org/publicmain/gui/ResultWindow.java
+++ publicMAIN/src/org/publicmain/gui/ResultWindow.java
@@ -4,16 +4,29 @@
 package org.publicmain.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLClientInfoException;
+import java.sql.Time;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextPane;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 
+import org.publicmain.chatengine.ChatEngine;
+import org.publicmain.common.LogEngine;
+import org.publicmain.common.MSG;
+import org.publicmain.common.MSGCode;
+import org.publicmain.common.Node;
 import org.resources.Help;
 
 /**
@@ -24,6 +37,12 @@ public class ResultWindow extends JDialog {
 	
 	private JTable resultTable;
 	private JScrollPane scroller;
+	private JButton autoSizeButton;
+	
+	private JTextPane msgTextPane;
+	private HTMLEditorKit htmlKit;
+	private HTMLDocument htmlDoc;
+	private JScrollPane hisScroller;
 	
 	public ResultWindow(JTable result){
 		this.resultTable = result;
@@ -39,15 +58,8 @@ public class ResultWindow extends JDialog {
 				
 			}
 		});
-		this.add(scroller);
 		
-		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		this.setTitle("Found " + resultTable.getRowCount() + " messages");
-		this.setIconImage(Help.getIcon("pM_Logo.png",64).getImage());
-//		this.setMinimumSize(new Dimension(750, 200));
-		this.pack();
-		this.setLocationRelativeTo(null);
-		 JButton autoSizeButton = new JButton("Auto-size columns");
+		this.autoSizeButton = new JButton("Auto-size columns");
 		 
 	        // resize the columns when the user clicks the button
 	        autoSizeButton.addActionListener(new ActionListener() {
@@ -56,6 +68,15 @@ public class ResultWindow extends JDialog {
 	            }
 	        });
 	        this.add(autoSizeButton,BorderLayout.SOUTH);
+		
+		
+		this.add(scroller);
+		
+		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		this.setTitle("Found " + resultTable.getRowCount() + " messages");
+		this.setIconImage(Help.getIcon("pM_Logo.png",64).getImage());
+		this.pack();
+		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 		
 		/*
@@ -69,5 +90,43 @@ public class ResultWindow extends JDialog {
 		 */
 	}
 
+	public ResultWindow(Object[][] history){
+		this.msgTextPane 	= new JTextPane();
+		this.htmlKit 		= new HTMLEditorKit();
+		this.htmlDoc 		= new HTMLDocument();
+		this.hisScroller 	= new JScrollPane(msgTextPane,	JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		this.msgTextPane.setEditable(false);
+		this.msgTextPane.setPreferredSize(new Dimension(400, 300));
+		this.msgTextPane.setEditorKit(htmlKit);
+		this.msgTextPane.setDocument(htmlDoc);
+		
+		for(int i=0; i<history.length; i++)
+		{
+			printHis(history[i]);
+		}
+		
+		this.add(hisScroller);
+		
+		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		this.setIconImage(Help.getIcon("pM_Logo.png",64).getImage());
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
 	
+	
+	private void printHis(Object[] history) {
+		try {
+			Time time = new Time((long)history[0]);
+			String senderName = (String)history[1];
+			String msg	= (String)history[2];
+			
+			htmlKit.insertHTML(htmlDoc, htmlDoc.getLength(), "<font color='orange'>" + time.toString() + " " + senderName +": </font><font color='black'>" + msg + "</font>", 0, 0, null);
+		} catch (BadLocationException | IOException e) {
+			LogEngine.log(e);
+		}
+		
+		msgTextPane.setCaretPosition(htmlDoc.getLength());
+	}
 }

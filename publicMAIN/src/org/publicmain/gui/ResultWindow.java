2e2496e977d70382e63e8432995c51b632c939b3
aberthold <arth004@gmail.com>
2013-05-02 11:41:11 +0200

    aaaaaaaaaaaaaaaaaaaarghs

--- publicMAIN/src/org/publicmain/gui/ResultWindow.java
+++ publicMAIN/src/org/publicmain/gui/ResultWindow.java
@@ -3,11 +3,19 @@
  */
 package org.publicmain.gui;
 
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
 
 import org.resources.Help;
 
@@ -24,7 +32,7 @@ public class ResultWindow extends JDialog {
 		this.resultTable = result;
 		this.scroller = new JScrollPane(resultTable);
 		
-		this.resultTable.setPreferredScrollableViewportSize(new Dimension(500, 70));
+//		this.resultTable.setPreferredScrollableViewportSize(new Dimension(500, 70));
 		this.resultTable.setFillsViewportHeight(true);
 		
 		this.add(scroller);
@@ -32,12 +40,11 @@ public class ResultWindow extends JDialog {
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		this.setTitle("Found " + resultTable.getRowCount() + " messages");
 		this.setIconImage(Help.getIcon("pM_Logo.png",64).getImage());
-		this.setMinimumSize(new Dimension(750, 200));
+//		this.setMinimumSize(new Dimension(750, 200));
 		this.pack();
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
-	
-	
+
 	
 }
